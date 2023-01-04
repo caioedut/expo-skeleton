2ddf255047a196f69a785fc85bdb91c95ea3c6bf
Caio Teixeira <caioedut@gmail.com>
2023-01-04 14:25:38 -0300

    Layout with React Bulk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,12 @@ import ReactBulk from '@react-bulk/core';
 import light from '../src/themes/light';
 import useScreenOptions from '../src/hooks/useScreenOptions';
 
-export default function Layout() {
-  return (
-    <ReactBulk theme={light}>
-      <Stack screenOptions={useScreenOptions()} />
-    </ReactBulk>
-  );
+function Layout() {
+  return <Stack screenOptions={useScreenOptions()} />;
 }
+
+export default () => (
+  <ReactBulk theme={light}>
+    <Layout />
+  </ReactBulk>
+);
