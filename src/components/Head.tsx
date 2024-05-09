1146d508ed6c7684079e0a4935ccb812a9ee762f
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-05-09 12:07:09 -0300

    feat: component Head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,22 +1,38 @@
 import { useEffect } from 'react';
 
-import { useNavigation } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { useTheme } from '@react-bulk/core';
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
+import { useFocusEffect, useLocalSearchParams, useNavigation } from 'expo-router';
+import { StatusBar, StatusBarStyle, setStatusBarStyle } from 'expo-status-bar';
 
-export type HeaderProps = {
-  title?: string;
-};
+import { string } from '@/helpers/string.helper';
 
-export default function Head({ title }: HeaderProps) {
+export default function Head({ title, headerShown = true, ...rest }: NativeStackNavigationOptions) {
+  const theme = useTheme();
+  const params = useLocalSearchParams();
   const navigation = useNavigation();
 
+  const statusBarStyle = theme.contrast('primary', 'light', 'dark') as StatusBarStyle;
+
   useEffect(() => {
-    navigation.setOptions({
-      title,
-    });
+    const options: NativeStackNavigationOptions = {
+      title: string(params?.title ?? title),
+      headerShown,
+      headerStyle: {
+        backgroundColor: theme.color('primary'),
+      },
+      headerTintColor: theme.contrast('primary'),
+      ...rest,
+    };
+
+    navigation.setOptions(options);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [title]);
+  }, [params?.title, title, headerShown, rest]);
 
-  return <StatusBar style="light" />;
+  useFocusEffect(() => {
+    setStatusBarStyle(statusBarStyle);
+  });
+
+  return <StatusBar style={statusBarStyle} />;
 }
